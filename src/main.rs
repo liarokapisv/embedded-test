29bf7884becc1e8e604f67cbc7b0e56cb87df44a
Daniel Henry-Mantilla <134883366+danielhenrymantilla-codespace@users.noreply.github.com>
2024-04-06 20:15:27 +0000

    An alternative design

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,15 @@
 
 mod flow;
 
-use core::pin::Pin;
+use core::pin::{Pin, pin};
+// use embassy_stm32::pac::sai::vals::Comp;
 use embassy_time::Duration;
 use embassy_time::Timer;
 
 use embassy_executor::Spawner;
 use embassy_stm32::init;
-use flow::{Flow, FlowCollector, FlowHandle};
-use pinned_init::*;
-use singleton_cell::new_singleton;
+use flow::{Ring, RingCollector, RingNode, RingNodeExt, RingNodeWithData};
 use {defmt_rtt as _, panic_probe as _};
-
-new_singleton!(pub Token);
 
 enum Event {
     Event,
@@ -41,41 +38,49 @@ impl Handler2 {
     }
 }
 
-impl<'c> FlowCollector<Token, Event, Context<'c>> for Handler1 {
-    fn emit(&mut self, _token: &mut Token, _value: &Event, context: &mut Context) {
+impl<'c> RingCollector<Event, Context<'c>> for Handler1 {
+    fn emit(self: Pin<&mut Self>, _value: &Event, context: &mut Context) {
         context.counter += 2;
         *context.x += 1;
     }
 }
 
-impl<'c> FlowCollector<Token, Event, Context<'c>> for Handler2 {
-    fn emit(&mut self, _token: &mut Token, _value: &Event, context: &mut Context) {
+impl<'c> RingCollector<Event, Context<'c>> for Handler2 {
+    fn emit(self: Pin<&mut Self>, _value: &Event, context: &mut Context) {
         context.counter -= 1;
         *context.x += 1;
     }
 }
 
-#[pin_data]
+#[::pin_project::pin_project]
 struct ComponentA<'h, 'c> {
     #[pin]
-    handle1: FlowHandle<'h, Token, Event, Context<'c>, Handler1>,
+    handle1: RingNodeWithData<'h, Event, Context<'c>, Handler1>,
     #[pin]
-    handle2: FlowHandle<'h, Token, Event, Context<'c>, Handler2>,
+    handle2: RingNodeWithData<'h, Event, Context<'c>, Handler2>,
 }
 
 impl<'h, 'c> ComponentA<'h, 'c> {
-    fn new<'f>(flow: Pin<&'f Flow<'h, Token, Event, Context<'c>>>) -> impl PinInit<Self> + 'f
-    where
-        'c: 'h,
+    fn new() -> Self {
+        Self {
+            handle1: RingNodeWithData::new(Handler1()),
+            handle2: RingNodeWithData::new(Handler2()),
+        }
+    }
+
+    fn init<'f>(
+        self: Pin<&Self>,
+        ring: Pin<&'f Ring<'h, Event, Context<'c>>>,
+    )
     {
-        pin_init!(Self {
-            handle1 <- FlowHandle::new(flow, Handler1()),
-            handle2 <- FlowHandle::new(flow, Handler2())
-        })
+        let ring_node = ring.node_links();
+        let this = self.project_ref();
+        this.handle1.insert_before(ring_node.as_ref());
+        this.handle2.insert_before(ring_node);
     }
 }
 
-#[pin_data]
+#[::pin_project::pin_project]
 struct ComponentB<'h, 'c> {
     #[pin]
     comp1: ComponentA<'h, 'c>,
@@ -84,41 +89,50 @@ struct ComponentB<'h, 'c> {
 }
 
 impl<'h, 'c> ComponentB<'h, 'c> {
-    fn new<'f>(flow: Pin<&'f Flow<'h, Token, Event, Context<'c>>>) -> impl PinInit<Self> + 'f
-    where
-        'c: 'h,
+    fn new() -> Self {
+        Self {
+            comp1: ComponentA::new(),
+            comp2: ComponentA::new(),
+        }
+    }
+
+    fn init(
+        self: Pin<&Self>,
+        ring: Pin<&Ring<'h, Event, Context<'c>>>,
+    )
     {
-        pin_init!(Self {
-            comp1 <- ComponentA::new(flow),
-            comp2 <- ComponentA::new(flow),
-        })
+        let this = self.project_ref();
+        this.comp1.init(ring.as_ref());
+        this.comp2.init(ring.as_ref());
     }
 }
 
-#[embassy_executor::main]
+#[::embassy_executor::main]
 async fn main(mut _spawner: Spawner) {
     let _p = init(Default::default());
 
-    let mut token = Token::new().expect("only created once!");
+    // let mut token = Token::new().expect("only created once!");
     let mut x: u32 = 0;
     let mut context = Context {
         counter: 0,
         x: &mut x,
     };
 
-    stack_pin_init!(let flow = Flow::<Token, Event, Context>::new());
-    stack_pin_init!(let component = ComponentB::new(flow.as_ref()));
+    let mut flow = pin!(Ring::<Event, Context>::new());
+    flow.as_ref().init();
+    let mut component = pin!(ComponentB::new());
+    component.as_ref().init(flow.as_ref());
 
     loop {
-        flow.emit(&mut token, &Event::Event, &mut context);
+        flow.as_mut().emit(&Event::Event, &mut context);
         defmt::info!("counter = {}", context.counter);
         Timer::after(Duration::from_millis(1000)).await;
-        component.comp1.handle1.borrow_mut(&mut token).extra();
-        component.comp1.handle2.borrow_mut(&mut token).extra();
+        component.as_mut().project().comp1.project().handle1.data_mut().extra();
+        component.as_mut().project().comp1.project().handle2.data_mut().extra();
         defmt::info!("counter = {}", context.counter);
         Timer::after(Duration::from_millis(1000)).await;
-        component.comp1.handle1.borrow_mut(&mut token).extra();
-        component.comp1.handle2.borrow_mut(&mut token).extra();
+        component.as_mut().project().comp1.project().handle1.data_mut().extra();
+        component.as_mut().project().comp1.project().handle2.data_mut().extra();
         defmt::info!("counter = {}", context.counter);
         Timer::after(Duration::from_millis(1000)).await;
     }
